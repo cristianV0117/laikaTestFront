ec4946569589a07760addcf60665e73fa3ba4095
cristian <cvo6372@gmail.com>
2021-11-28 12:59:47 -0500

    Add method insert user

--- public/js/users.js
+++ public/js/users.js
@@ -11,7 +11,40 @@ class Users
     {
         document.getElementById('registerUsersForm').addEventListener('submit', event => {
             event.preventDefault()
-            alert("hola")
+				const data = {
+					'user_name': document.getElementById('user_name').value,
+                    'first_name': document.getElementById('first_name').value,
+                    'second_name': document.getElementById('second_name').value,
+                    'first_last_name': document.getElementById('first_last_name').value,
+                    'second_last_name': document.getElementById('second_last_name').value,
+                    'email': document.getElementById('email').value,
+                    'cellphone': document.getElementById('cellphone').value,
+					'password': document.getElementById('password').value,
+                    'country_id': document.getElementById('country_id').value,
+				}
+                fetch(this.route, {
+                    method: 'POST',
+                    headers: {
+                        'authorization': "601b422c-786f-463c-8eae-971a7bf08f92",
+                        'Accept': 'application/json',
+                		'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                    .then(json => json.json())
+                    .then(response => {
+                        console.log(response.status)
+                        if (response.status == 201) {
+                            this.alert.innerHTML = `<hr /><div class="alert alert-success" role="alert">
+                                Se ha insertado el usuario ${response.message[0].user_name}
+                            </div>`;
+                            setTimeout(() => {
+                                location.replace("/");
+                            }, 1500);
+                        } else {
+                            console.log(response.message)
+                        }
+                    })
         });
     }
 
@@ -32,7 +65,7 @@ class Users
                 } else {
                     if (data.message.length > 0) {
                         data.message.forEach(element => {
-                            this.body.innerHTML = `<tr>
+                            this.body.innerHTML += `<tr>
                                 <td>${element.id}</td>
                                 <td>${element.user_name}</td>
                                 <td>${element.first_name}</td>
